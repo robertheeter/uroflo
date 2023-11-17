cd8df7ba67e5102c388baba14e4228ccf5752484
rcheeter <robert.heeter@rice.edu>
2023-11-17 12:18:24 -0600

    Update compression.py

--- compression/compression.py
+++ compression/compression.py
@@ -9,17 +9,17 @@ https://www.electronicshub.org/raspberry-pi-l298n-interface-tutorial-control-dc-
 import RPi.GPIO as GPIO          
 from time import sleep
 
-in1 = 24
-in2 = 23
+in1 = 23
+in2 = 24
 en = 25
-temp1=1
+temp1 = 1
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(en, GPIO.OUT)
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
 
 p=GPIO.PWM(en,1000)
 p.start(25)
@@ -52,25 +52,25 @@ while(True):
         print("forward")
         GPIO.output(in1,GPIO.HIGH)
         GPIO.output(in2,GPIO.LOW)
-        temp1=1
+        temp1 = 1
         x='z'
     elif x=='b':
         print("backward")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.HIGH)
-        temp1=0
+        temp1 = 0
         x='z'
     elif x=='l':
         print("low")
-        p.ChangeDutyCycle(25)
+        p.ChangeDutyCycle(20)
         x='z'
     elif x=='m':
         print("medium")
-        p.ChangeDutyCycle(50)
+        p.ChangeDutyCycle(60)
         x='z'
     elif x=='h':
         print("high")
-        p.ChangeDutyCycle(75)
+        p.ChangeDutyCycle(100)
         x='z'
     elif x=='e':
         GPIO.cleanup()
