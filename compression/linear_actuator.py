92bdaff06fa0a8b8b5d927fe3b7d28eb904b2a7c
rcheeter <robert.heeter@rice.edu>
2023-11-18 18:24:02 -0600

    Update docs

---
+++ compression/linear_actuator.py
@@ -0,0 +1,89 @@
+'''
+LINEAR ACTUATOR
+- L298N driver
+- 12 V linear actuator (1.2 in stroke, 0.6 in/s, 60 N / 14 lb)
+
+Documentation
+https://www.electronicshub.org/raspberry-pi-l298n-interface-tutorial-control-dc-motor-l298n-raspberry-pi/
+
+'''
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+class LinearActuator():
+    def __init__(self, x):
+        self.x = x
+
+    def x():
+        
+
+in1 = 23
+in2 = 24
+en = 25
+temp1 = 1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(en, GPIO.OUT)
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+
+p=GPIO.PWM(en,1000)
+p.start(25)
+
+print("\n")
+print("The default speed & direction of motor is LOW & Forward.....")
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+print("\n")
+
+while(True):
+    x=input()
+    if x=='r':
+        print("run")
+        if(temp1==1):
+            GPIO.output(in1,GPIO.HIGH)
+            GPIO.output(in2,GPIO.LOW)
+            print("forward")
+            x='z'
+        else:
+            GPIO.output(in1,GPIO.LOW)
+            GPIO.output(in2,GPIO.HIGH)
+            print("backward")
+            x='z'
+    elif x=='s':
+        print("stop")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        x='z'
+    elif x=='f':
+        print("forward")
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+        temp1 = 1
+        x='z'
+    elif x=='b':
+        print("backward")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+        temp1 = 0
+        x='z'
+    elif x=='l':
+        print("low")
+        p.ChangeDutyCycle(10)
+        x='z'
+    elif x=='m':
+        print("medium")
+        p.ChangeDutyCycle(50)
+        x='z'
+    elif x=='h':
+        print("high")
+        p.ChangeDutyCycle(90)
+        x='z'
+    elif x=='e':
+        GPIO.cleanup()
+        break
+    else:
+        print("<<<  wrong data  >>>")
+        print("please enter the defined data to continue.....")
---
+++ hematuria/hematuria_test.py
@@ -0,0 +1,57 @@
+'''
+SPECTRAL SENSOR
+
+Documentation
+https://docs.circuitpython.org/projects/as726x/en/latest/api.html#adafruit_as726x.AS726x.read_calibrated_value
+
+Python GitHub
+https://github.com/adafruit/Adafruit_CircuitPython_AS726x
+
+Visible Datasheet
+https://cdn.sparkfun.com/assets/f/b/c/c/f/AS7262.pdf?_gl=1*12hia0d*_ga*ODIxNjU4Nzk4LjE3MDAyMDk5MjU.*_ga_T369JS7J9N*MTcwMDIwOTkyNS4xLjAuMTcwMDIwOTkyNS42MC4wLjA.
+
+NIR Datasheet
+https://cdn.sparkfun.com/assets/1/b/7/3/b/AS7263.pdf?_gl=1*1v54ztc*_ga*ODIxNjU4Nzk4LjE3MDAyMDk5MjU.*_ga_T369JS7J9N*MTcwMDIwOTkyNS4xLjEuMTcwMDIxMDM2NS42MC4wLjA.
+
+Arduino GitHub
+https://github.com/adafruit/Adafruit_AS726x/tree/master
+
+'''
+
+import time
+import board
+
+from adafruit_as726x import AS726x_I2C
+
+sensor_type = 'VIS'
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+sensor = AS726x_I2C(i2c)
+sensor.conversion_mode = sensor.MODE_2
+
+print('Temperature: {0}C'.format(sensor.temperature))
+
+while True:
+
+    # wait for sensor to be ready
+    while not sensor.data_ready:
+        time.sleep(1)
+
+    print("\n")
+    if sensor_type == 'VIS':
+        print("V: " + str(sensor.violet)) # 450 nm
+        print("B: " + str(sensor.blue)) # 500 nm
+        print("G: " + str(sensor.green)) # 550 nm
+        print("Y: " + str(sensor.yellow)) # 570 nm
+        print("O: " + str(sensor.orange)) # 600 nm
+        print("R: " + str(sensor.red)) # 650 nm
+
+    elif sensor_type == 'NIR':
+        print("610: " + str(sensor.violet)) # 610 nm
+        print("680: " + str(sensor.blue)) # 680 nm
+        print("730: " + str(sensor.green)) # 730 nm
+        print("760: " + str(sensor.yellow)) # 760 nm
+        print("810: " + str(sensor.orange)) # 810 nm
+        print("860: " + str(sensor.red)) # 860 nm
+
+    time.sleep(1)
