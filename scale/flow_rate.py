cdefc7bfa8dc9077c53741465565d1cfcc01fd31
achabbi <aac17@rice.edu>
2023-11-21 01:02:42 -0600

    Updated flow rate calculation

--- scale/flow_rate.py
+++ scale/flow_rate.py
@@ -1,14 +1,36 @@
 import time
+from statistics import mean
 from scale_class import Scale
 
-
 if __name__ == '__main__':
+
     scale = Scale(pd_sck_pin=2, dout_pin=3)
+
+    # window size for moving average (number of data points)
+    window = 500
+
+    # frequency of flow rate measurement (seconds)
+    freq = 60
+
+    start_time = time.time()
+    data = [scale.get_weight() for _ in range(window)]
+    start_weight = mean(data)
 
     while True:
         try:
-            # calculate flow rate every 60 seconds
-            flow_rate = scale.calculate_flow_rate(60)
-            print(f'Flow rate: {flow_rate} mL/min')
+            reading = scale.get_weight()
+            data.append(reading)
+            avg_weight = mean(data)
+            data.pop(0)
+
+            if time.time() - start_time >= freq:
+                flow_rate = ((start_weight - avg_weight)/freq)*60
+                print(f'Flow rate: {flow_rate} mL/min')
+
+                start_time = time.time()
+                start_weight = avg_weight
+            
+            time.sleep(0.1) # wait 0.1 seconds between weight measurements
+
         except (KeyboardInterrupt, SystemExit):
             scale.stop()
--- scale/scale_class.py
+++ scale/scale_class.py
@@ -26,36 +26,36 @@ class Scale:
         time.sleep(0.1)
         return weight
     
-    def calculate_flow_rate(self, freq=60):
-        start_time = time.time()
-        current_time = time.time()
+#    def calculate_flow_rate(self, freq=60):
+#        start_time = time.time()
+#        current_time = time.time()
 
-        start_weight = 0
-        end_weight = 0
-        weights = []
-        calculate_start_weight = False
+#        start_weight = 0
+#        end_weight = 0
+#        weights = []
+#        calculate_start_weight = False
 
         # calculate flow rate once every cycle (set by frequency)
-        while current_time - start_time <= freq:
-            current_time = time.time()
-            reading = self.read_weight() # gets weight reading from hx711
+#        while current_time - start_time <= freq:
+#            current_time = time.time()
+#            reading = self.read_weight() # gets weight reading from hx711
 
-            if current_time - start_time <= 1:
-                weights.append(reading)
+#            if current_time - start_time <= 1:
+#                weights.append(reading)
                 
-            if current_time - start_time > 1 and not calculate_start_weight:
-                start_weight = mean(weights)
-                weights = []
-                calculate_start_weight = True
+#            if current_time - start_time > 1 and not calculate_start_weight:
+#                start_weight = mean(weights)
+#                weights = []
+#                calculate_start_weight = True
 
-            if current_time - start_time >= freq - 1 and current_time - start_time < freq:
-                weights.append(reading)
+#            if current_time - start_time >= freq - 1 and current_time - start_time < freq:
+#                weights.append(reading)
 
-            if current_time - start_time >= freq:
-                end_weight = mean(weights)
-                weight_change = start_weight - end_weight
-                flow_rate = (weight_change/(current_time - start_time))*60
-                return flow_rate
+#            if current_time - start_time >= freq:
+#                end_weight = mean(weights)
+#                weight_change = start_weight - end_weight
+#                flow_rate = (weight_change/(current_time - start_time))*60
+#                return flow_rate
         
     def stop(self):
         print('Stopped')
