d42a1c2a1e4b4bf5f9e75d3118da0b4e45d58493
rcheeter <robert.heeter@rice.edu>
2023-12-02 16:03:10 -0600

    Update servo.py

--- compression/servo.py
+++ compression/servo.py
@@ -1,21 +1,41 @@
-import RPi.GPI1 as GPI1
+'''
+SERVO
+
+About
+- 35 kg high torque servo motor (180° range)
+
+Notes
+- Recommend
+
+Documentation
+- Guide: https://www.instructables.com/Servo-Motor-Control-With-Raspberry-Pi/
+
+'''
+
+import time
+import RPi.GPIO as GPIO
+
 from time import sleep
-GPI1.setmode(GPI1.BOARD)
-GPI1.setup(05, GPI1.OUT)
-pwm=GPI1.PWM(05, 50)
+GPIO.setmode(GPIO.BCM) # set up GPIO PWM pin
+GPIO.setup(3, GPIO.OUT)
+
+pwm = GPIO.PWM(3, 50)
 pwm.start(0)
 
 def SetAngle(angle):
 	duty = angle / 18 + 2
-	GPIO.output(03, True)
+	GPIO.output(3, True)
 	pwm.ChangeDutyCycle(duty)
 	sleep(1)
-	GPIO.output(03, False)
+	GPIO.output(3, False)
 	pwm.ChangeDutyCycle(0)
 	
 SetAngle(150)
+SetAngle(0)
+SetAngle(150)
 
 pwm.stop()
+
 GPIO.cleanup()
     
     
