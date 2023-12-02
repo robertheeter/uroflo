07c77e4284f1504abe6734279f0571efccdc56b4
rcheeter <robert.heeter@rice.edu>
2023-12-02 16:05:00 -0600

    Update servo.py

--- compression/servo.py
+++ compression/servo.py
@@ -17,17 +17,17 @@ import RPi.GPIO as GPIO
 
 from time import sleep
 GPIO.setmode(GPIO.BCM) # set up GPIO PWM pin
-GPIO.setup(3, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 
-pwm = GPIO.PWM(3, 50)
+pwm = GPIO.PWM(4, 50)
 pwm.start(0)
 
 def SetAngle(angle):
 	duty = angle / 18 + 2
-	GPIO.output(3, True)
+	GPIO.output(4, True)
 	pwm.ChangeDutyCycle(duty)
 	sleep(1)
-	GPIO.output(3, False)
+	GPIO.output(4, False)
 	pwm.ChangeDutyCycle(0)
 	
 SetAngle(150)
