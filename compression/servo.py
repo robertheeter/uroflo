65d464fbec82168ec8b282266cce5dbcdec12255
rcheeter <robert.heeter@rice.edu>
2023-12-02 16:32:20 -0600

    Update servo.py

--- compression/servo.py
+++ compression/servo.py
@@ -22,26 +22,33 @@ GPIO.setup(4, GPIO.OUT)
 pwm = GPIO.PWM(4, 50)
 pwm.start(0)
 
-def SetAngle(angle):
-	print(f"angle: {angle}")
-	duty = angle / 18 + 2.5
-	GPIO.output(4, True)
-	pwm.ChangeDutyCycle(duty)
-	sleep(1)
-	GPIO.output(4, False)
-	pwm.ChangeDutyCycle(0)
-	
-SetAngle(0) # 0 is completely open
-time.sleep(2)
-SetAngle(180)
-time.sleep(2)
-SetAngle(90)
-time.sleep(2)
-SetAngle(180)
-time.sleep(2)
-SetAngle(0)
 
-time.sleep(2)
+duty = 2
+while duty <= 17:
+	pwm.ChangeDutyCycle(duty)
+	time.sleep(1)
+	duty += 1	
+
+# def SetAngle(angle):
+# 	print(f"angle: {angle}")
+# 	duty = angle / 18 + 2.5
+# 	GPIO.output(4, True)
+# 	pwm.ChangeDutyCycle(duty)
+# 	sleep(1)
+# 	GPIO.output(4, False)
+# 	pwm.ChangeDutyCycle(0)
+	
+# SetAngle(0) # 0 is completely open
+# time.sleep(2)
+# SetAngle(180)
+# time.sleep(2)
+# SetAngle(90)
+# time.sleep(2)
+# SetAngle(180)
+# time.sleep(2)
+# SetAngle(0)
+
+# time.sleep(2)
 
 pwm.stop()
 
