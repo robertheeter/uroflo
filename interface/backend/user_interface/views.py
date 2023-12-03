37a3570d86909a5e592744c0b4fe1f5f874dcc5a
rcheeter <robert.heeter@rice.edu>
2023-12-03 11:22:59 -0600

    Update views.py

--- interface/backend/user_interface/views.py
+++ interface/backend/user_interface/views.py
@@ -1,10 +1,10 @@
 from django.http import JsonResponse
 import random
 
-from .device.spectral_sensor import SpectralSensor
+# from .device.spectral_sensor import SpectralSensor
 from .device.weight_sensor import WeightSensor
 
-spectral_sensor = SpectralSensor()
+# spectral_sensor = SpectralSensor()
 weight_sensor = WeightSensor(pd_sck_pin=19, dout_pin=26)
 
 def get_hematuria(request):
