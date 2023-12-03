bbe0d588f2b01056c382ef5b9960bb9526d1bf73
rcheeter <robert.heeter@rice.edu>
2023-12-02 20:23:39 -0600

    Update settings.py

--- interface/uroflo_site/core/settings.py
+++ interface/uroflo_site/core/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'interface.apps.InterfaceConfig' # add interface app
+    # 'interface.apps.InterfaceConfig' # add interface app
     'corsheaders', # add CORS
 ]
 
