ce6e5527fb2d820ea546275391c8b5d2d8196097
rcheeter <robert.heeter@rice.edu>
2023-12-03 01:12:32 -0600

    yeet

--- interface/uroflo_django/user_interface/urls.py
+++
@@ -1,8 +0,0 @@
-# myapp/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('get_hematuria/', views.get_hematuria, name='get_hematuria'),
-    path('get_saline_weight/', views.get_saline_weight, name='get_saline_weight'),
-]
