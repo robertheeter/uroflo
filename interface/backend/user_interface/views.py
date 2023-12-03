fd5533d3ca52eb805f1f352ddad8980ce522b455
rcheeter <robert.heeter@rice.edu>
2023-12-03 15:37:11 -0600

    Update views.py

--- interface/backend/user_interface/views.py
+++ interface/backend/user_interface/views.py
@@ -13,14 +13,13 @@ def get_hematuria(request):
     level = random.randint(0, 100)
     color = [1,2,3]
 
-    return JsonResponse({'level': level, 'color': color})
+    return JsonResponse({'level': int(level), 'color': color})
 
 def get_supply(request):
     volume = weight_sensor.mass() / 1009 # convert mg to L with density
     percent = int(min((volume/3)*100, 100)) # using 3 L bag
-    volume = round(volume, 2)
 
     rate = random.randint(0, 100) # random integer for noww
     time = random.randint(0, 100) # random integer for now
 
-    return JsonResponse({'volume': volume, 'rate': rate, 'percent': percent, 'time': time})
+    return JsonResponse({'volume': format(volume, '.1f'), 'rate': format(rate, '.1f'), 'percent': int(percent), 'time': int(time)})
