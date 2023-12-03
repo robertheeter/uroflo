c8870db8d9ed6a1f7090e05157b74716e078ab32
rcheeter <robert.heeter@rice.edu>
2023-12-03 01:45:25 -0600

    Update urls.py

--- interface/backend/user_interface/urls.py
+++ interface/backend/user_interface/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('get_hematuria/', views.get_hematuria, name='get_hematuria'),
-    path('get_saline_weight/', views.get_saline_weight, name='get_saline_weight'),
+    path('get_supply/', views.get_supply, name='get_supply'),
 ]
