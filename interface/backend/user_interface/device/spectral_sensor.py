11c91113232ff402724cf8b90513694bcb08f3eb
rcheeter <robert.heeter@rice.edu>
2023-12-03 11:29:43 -0600

    banana

--- interface/backend/user_interface/device/spectral_sensor.py
+++ interface/backend/user_interface/device/spectral_sensor.py
@@ -103,81 +103,76 @@ class SpectralSensor():
         GPIO.cleanup()
 
 
+# import pandas as pd
 
+# # testing
+# if __name__ == '__main__':
 
+#     sensor_type = 'VIS'
+#     n = 5
 
+#     if sensor_type in ['VIS', 'AS7262']:
+#         max = 48000
+#     elif sensor_type in ['NIR', 'AS7263']:
+#         max = 16000
 
+#     ss = SpectralSensor(led_pin=4, sensor_type=sensor_type, range=[0, 100], max=max, verbose=False)
 
-import pandas as pd
-
-# testing
-if __name__ == '__main__':
-
-    sensor_type = 'VIS'
-    n = 5
-
-    if sensor_type in ['VIS', 'AS7262']:
-        max = 48000
-    elif sensor_type in ['NIR', 'AS7263']:
-        max = 16000
-
-    ss = SpectralSensor(led_pin=4, sensor_type=sensor_type, range=[0, 100], max=max, verbose=False)
-
-    trials = []
-    for i in range(n):
-        readings = []
-        for j in range(10):
-            readings.append(ss.read(use_led=True))
-            time.sleep(0.1)
+#     trials = []
+#     for i in range(n):
+#         readings = []
+#         for j in range(10):
+#             readings.append(ss.read(use_led=True))
+#             time.sleep(0.1)
         
-        df = pd.DataFrame(readings)
-        avg_readings = dict(df.mean())
-        trials.append(avg_readings)
+#         df = pd.DataFrame(readings)
+#         avg_readings = dict(df.mean())
+#         trials.append(avg_readings)
     
-    df = pd.DataFrame(trials)
-    avg_trials = dict(df.mean())
+#     df = pd.DataFrame(trials)
+#     avg_trials = dict(df.mean())
 
-    print(f"\nAVERAGE READINGS (n={n}):\n")
+#     print(f"\nAVERAGE READINGS (n={n}):\n")
 
-    if sensor_type in ['VIS', 'AS7262']:
-        print('450 nm / violet : {:.2f}'.format(avg_trials[450]))
-        print('500 nm / blue   : {:.2f}'.format(avg_trials[500]))
-        print('550 nm / green  : {:.2f}'.format(avg_trials[550]))
-        print('570 nm / yellow : {:.2f}'.format(avg_trials[570]))
-        print('600 nm / orange : {:.2f}'.format(avg_trials[600]))
-        print('650 nm / red    : {:.2f}'.format(avg_trials[650]))
+#     if sensor_type in ['VIS', 'AS7262']:
+#         print('450 nm / violet : {:.2f}'.format(avg_trials[450]))
+#         print('500 nm / blue   : {:.2f}'.format(avg_trials[500]))
+#         print('550 nm / green  : {:.2f}'.format(avg_trials[550]))
+#         print('570 nm / yellow : {:.2f}'.format(avg_trials[570]))
+#         print('600 nm / orange : {:.2f}'.format(avg_trials[600]))
+#         print('650 nm / red    : {:.2f}'.format(avg_trials[650]))
 
-    elif sensor_type in ['NIR', 'AS7263']:
-        print('610 nm / orange : {:.2f}'.format(avg_trials[610]))
-        print('680 nm / red    : {:.2f}'.format(avg_trials[680]))
-        print('730 nm / IR     : {:.2f}'.format(avg_trials[730]))
-        print('760 nm / IR     : {:.2f}'.format(avg_trials[760]))
-        print('810 nm / IR     : {:.2f}'.format(avg_trials[810]))
-        print('860 nm / IR     : {:.2f}'.format(avg_trials[860]))
+#     elif sensor_type in ['NIR', 'AS7263']:
+#         print('610 nm / orange : {:.2f}'.format(avg_trials[610]))
+#         print('680 nm / red    : {:.2f}'.format(avg_trials[680]))
+#         print('730 nm / IR     : {:.2f}'.format(avg_trials[730]))
+#         print('760 nm / IR     : {:.2f}'.format(avg_trials[760]))
+#         print('810 nm / IR     : {:.2f}'.format(avg_trials[810]))
+#         print('860 nm / IR     : {:.2f}'.format(avg_trials[860]))
 
-    df = pd.DataFrame(trials)
-    cov_trials = dict(df.std()/df.mean())
+#     df = pd.DataFrame(trials)
+#     cov_trials = dict(df.std()/df.mean())
 
-    print(f"\nCOEFFICIENT OF VARIATION READINGS (n={n}):\n")
+#     print(f"\nCOEFFICIENT OF VARIATION READINGS (n={n}):\n")
 
-    if sensor_type in ['VIS', 'AS7262']:
-        print('450 nm / violet : {:.4f}%'.format(cov_trials[450]*100))
-        print('500 nm / blue   : {:.4f}%'.format(cov_trials[500]*100))
-        print('550 nm / green  : {:.4f}%'.format(cov_trials[550]*100))
-        print('570 nm / yellow : {:.4f}%'.format(cov_trials[570]*100))
-        print('600 nm / orange : {:.4f}%'.format(cov_trials[600]*100))
-        print('650 nm / red    : {:.4f}%'.format(cov_trials[650]*100))
+#     if sensor_type in ['VIS', 'AS7262']:
+#         print('450 nm / violet : {:.4f}%'.format(cov_trials[450]*100))
+#         print('500 nm / blue   : {:.4f}%'.format(cov_trials[500]*100))
+#         print('550 nm / green  : {:.4f}%'.format(cov_trials[550]*100))
+#         print('570 nm / yellow : {:.4f}%'.format(cov_trials[570]*100))
+#         print('600 nm / orange : {:.4f}%'.format(cov_trials[600]*100))
+#         print('650 nm / red    : {:.4f}%'.format(cov_trials[650]*100))
 
-    elif sensor_type in ['NIR', 'AS7263']:
-        print('610 nm / orange : {:.4f}%'.format(cov_trials[610]*100))
-        print('680 nm / red    : {:.4f}%'.format(cov_trials[680]*100))
-        print('730 nm / IR     : {:.4f}%'.format(cov_trials[730]*100))
-        print('760 nm / IR     : {:.4f}%'.format(cov_trials[760]*100))
-        print('810 nm / IR     : {:.4f}%'.format(cov_trials[810]*100))
-        print('860 nm / IR     : {:.4f}%'.format(cov_trials[860]*100))
+#     elif sensor_type in ['NIR', 'AS7263']:
+#         print('610 nm / orange : {:.4f}%'.format(cov_trials[610]*100))
+#         print('680 nm / red    : {:.4f}%'.format(cov_trials[680]*100))
+#         print('730 nm / IR     : {:.4f}%'.format(cov_trials[730]*100))
+#         print('760 nm / IR     : {:.4f}%'.format(cov_trials[760]*100))
+#         print('810 nm / IR     : {:.4f}%'.format(cov_trials[810]*100))
+#         print('860 nm / IR     : {:.4f}%'.format(cov_trials[860]*100))
     
-    print("\n####################")
-    print(f"\nSTANDARD DEVIATION READINGS (n={n}):\n{df.std()}")
-    print(f"\nVARIANCE READINGS (n={n}):\n{df.var()}\n")
+#     print("\n####################")
+#     print(f"\nSTANDARD DEVIATION READINGS (n={n}):\n{df.std()}")
+#     print(f"\nVARIANCE READINGS (n={n}):\n{df.var()}\n")
 
     
--- interface/backend/user_interface/views.py
+++ interface/backend/user_interface/views.py
@@ -8,7 +8,7 @@ from .device.weight_sensor import WeightSensor
 weight_sensor = WeightSensor(pd_sck_pin=19, dout_pin=26)
 
 def get_hematuria(request):
-    level = random.randint(0, 100) # re
+    level = random.randint(0, 100)
     color = [1,2,3]
 
     return JsonResponse({'level': level, 'color': color})
@@ -16,16 +16,12 @@ def get_hematuria(request):
 def get_supply(request):
     volume = weight_sensor.volume()
     volume = volume / (1009)
-    volume = round(volume, 1)
-    percent = int((volume/3)*100) # assuming 3 L bag
-    print(volume)
-    print(percent)
+    int(min((volume/3)*100, 100)) # using 3 L bag
+    volume = int(round(volume, 1))
 
     # volume = random.randint(0, 100) # re
     rate = random.randint(0, 100) # re
-    percent = random.randint(0, 100) # re
+    # percent = random.randint(0, 100) # re
     time = random.randint(0, 100) # re
 
-    print(percent)
-    
     return JsonResponse({'volume': volume, 'rate': rate, 'percent': percent, 'time': time})
