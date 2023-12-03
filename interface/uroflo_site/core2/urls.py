54238a45de31d2268688ba21ce8d2841ba6bc5e8
rcheeter <robert.heeter@rice.edu>
2023-12-02 20:11:58 -0600

    Update interface

--- interface/uroflo_site/core2/urls.py
+++ interface/uroflo_site/core2/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for uroflo_site project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -13,13 +13,11 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-
 """
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user_interface/', include('user_interface.urls')),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
