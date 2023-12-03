a60030bbff12b8227924505548abfcb67ecad0cb
rcheeter <robert.heeter@rice.edu>
2023-12-02 20:40:44 -0600

    Update urls.py

--- interface/uroflo_site/core/urls.py
+++ interface/uroflo_site/core/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('interface/', include('interface.urls')),
+    path('user_interface/', include('user_interface.urls')),
 ]
