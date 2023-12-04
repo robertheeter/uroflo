86c7c55073f12f8c51513d4da06bab4f7ad2b375
rcheeter <robert.heeter@rice.edu>
2023-12-03 20:39:05 -0600

    Update views.py

--- interface/backend/user_interface/views.py
+++ interface/backend/user_interface/views.py
@@ -10,7 +10,7 @@ weight_sensor = WeightSensor(pd_sck_pin=14, dout_pin=15, verbose=True)
 def get_hematuria(request):
     
     max = 12 # 100% hematuria
-    weights = [-0.4198196025, 0.2050549854, -0.2104491625, 0.05050789723] # weights for visible spectrum in wavelength order of [450, 500, 550, 570]
+    weights = [0.0001052247859, -0.0004384357553, 0.0004271978863, -0.0008746241718] # weights for visible spectrum in wavelength order of [450, 500, 550, 570]
     bias = 28.34907833 # offset
 
     level = spectral_sensor.level(weights, bias, max, n=10, range=[0, 100])
@@ -25,7 +25,7 @@ def get_supply(request):
     volume = weight_sensor.mass() / 1009 # convert mg to L with density
     percent = int(min((volume/3)*100, 100)) # using 3 L bag
 
-    rate = random.randint(0, 100) # random integer for now
+    rate = random.randint(0, 100) # random integer for noww
     time = random.randint(0, 100) # random integer for now
 
     return JsonResponse({'volume': format(volume, '1.1f'),
