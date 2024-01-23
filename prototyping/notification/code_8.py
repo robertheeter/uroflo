1694a0f3729e24477c3a5c59bb6641263a65d55b
rcheeter <robert.heeter@rice.edu>
2024-01-22 21:39:40 -0600

    Update code_8.py

--- prototyping/notification/code_8.py
+++ prototyping/notification/code_8.py
@@ -27,32 +27,39 @@ def duty_cycle(percent):
     return int(percent / 100.0 * 65535.0)
 
 
-# Fade from nothing up to full red.
-for i in range(100):
-    red.duty_cycle = duty_cycle(i)
-    time.sleep(FADE_SLEEP / 1000)
+# # Fade from nothing up to full red.
+# for i in range(100):
+#     red.duty_cycle = duty_cycle(i)
+#     time.sleep(FADE_SLEEP / 1000)
 
-# Now fade from violet (red + blue) down to red.
-for i in range(100, -1, -1):
-    blue.duty_cycle = duty_cycle(i)
-    time.sleep(FADE_SLEEP / 1000)
+# # Now fade from violet (red + blue) down to red.
+# for i in range(100, -1, -1):
+#     blue.duty_cycle = duty_cycle(i)
+#     time.sleep(FADE_SLEEP / 1000)
 
-# Fade from red to yellow (red + green).
-for i in range(100):
-    green.duty_cycle = duty_cycle(i)
-    time.sleep(FADE_SLEEP / 1000)
+# # Fade from red to yellow (red + green).
+# for i in range(100):
+#     green.duty_cycle = duty_cycle(i)
+#     time.sleep(FADE_SLEEP / 1000)
 
-# Fade from yellow to green.
-for i in range(100, -1, -1):
-    red.duty_cycle = duty_cycle(i)
-    time.sleep(FADE_SLEEP / 1000)
+# # Fade from yellow to green.
+# for i in range(100, -1, -1):
+#     red.duty_cycle = duty_cycle(i)
+#     time.sleep(FADE_SLEEP / 1000)
 
-# Fade from green to teal (blue + green).
-for i in range(100):
-    blue.duty_cycle = duty_cycle(i)
-    time.sleep(FADE_SLEEP / 1000)
+# # Fade from green to teal (blue + green).
+# for i in range(100):
+#     blue.duty_cycle = duty_cycle(i)
+#     time.sleep(FADE_SLEEP / 1000)
 
-# Fade from teal to blue.
-for i in range(100, -1, -1):
-    green.duty_cycle = duty_cycle(i)
-    time.sleep(FADE_SLEEP / 1000)
+# # Fade from teal to blue.
+# for i in range(100, -1, -1):
+#     green.duty_cycle = duty_cycle(i)
+#     time.sleep(FADE_SLEEP / 1000)
+
+for i in [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0]:
+    blue.duty_cycle = i
+    green.duty_cycle = 0
+    red.duty_cycle = 0
+    print(i)
+    time.sleep(5)
