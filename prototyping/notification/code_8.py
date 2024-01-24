4d4882f8f0edf8afb3192e697d23b16300cf5503
UroFlo <150858412+teamuroflo@users.noreply.github.com>
2024-01-24 16:44:53 -0600

    Update code_8.py

--- prototyping/notification/code_8.py
+++ prototyping/notification/code_8.py
@@ -8,38 +8,56 @@ import board
 import pwmio
 import pygame
 
-# Mellow sound for bag changes 
-# playing sonar sound
-pygame.init()
-my_sound = pygame.mixer.Sound('sonar.mp3') # only plays once
-my_sound.play()
-my_sound.set_volume(0.5) # can set sound volume from 0-1 
-print('playing sonar sound')
-
-# Harsh sound for emergencies 
-my_sound = pygame.mixer.Sound('harsh.mp3') # repeats 4 times 
-my_sound.play()
-my_sound.set_volume(0.5) # can set sound volume from 0-1 
-print('playing harsh sound')
-
 # Defining pins on board GPIOs 
 RED_PIN = board.D8  # Red LED pin
 GREEN_PIN = board.D7  # Green LED pin
 BLUE_PIN = board.D1  # Blue LED pin
-
-FADE_SLEEP = 100  # Number of milliseconds to delay between changes.
-# Increase to slow down, decrease to speed up.
 
 # Define PWM outputs:
 red = pwmio.PWMOut(RED_PIN)
 green = pwmio.PWMOut(GREEN_PIN)
 blue = pwmio.PWMOut(BLUE_PIN)
 
-
 # Function to simplify setting duty cycle to percent value.
-
 def duty_cycle(percent):
     return int(percent / 100.0 * 65535.0)
+    
+# default standard blue color
+def default():
+    red.duty_cycle = duty_cycle(1)
+    green.duty_cycle = duty_cycle(2.74)
+    blue.duty_cycle = duty_cycle(4)
+
+# red - bad 
+def red():
+    red.duty_cycle = duty_cycle(50)
+
+# orange - medium 
+def orange():
+    red.duty_cycle = duty_cycle(30)
+    blue.duty_cycle = duty_cycle(1)
+    green.duty_cycle = duty_cycle(7)
+    
+# yellow - low
+def yellow():
+    red.duty_cycle = duty_cycle(20)
+    blue.duty_cycle = duty_cycle(0)
+    green.duty_cycle = duty_cycle(10)
+
+def mellow_alarn():
+    pygame.init()
+    my_sound = pygame.mixer.Sound('sonar.mp3') # only plays once
+    my_sound.play()
+    my_sound.set_volume(0.5) # can set sound volume from 0-1 
+
+def harsh_alarm():
+    my_sound = pygame.mixer.Sound('harsh.mp3') # repeats 4 times 
+    my_sound.play()
+    my_sound.set_volume(0.5) # can set sound volume from 0-1 
+    
+
+# FADE_SLEEP = 100  # Number of milliseconds to delay between changes.
+# # Increase to slow down, decrease to speed up.
 
 # while True:
 #     # Fade from nothing up to full red.
@@ -72,34 +90,7 @@ def duty_cycle(percent):
 #         green.duty_cycle = duty_cycle(i)
 #         time.sleep(FADE_SLEEP / 1000)
 
-# make functions for setting to particular color or RGB value?
 
-# Default color - uroflo blue
-# while True: 
-#     red.duty_cycle = duty_cycle(1)
-#     blue.duty_cycle = duty_cycle(4)
-#     green.duty_cycle = duty_cycle(2.74)
-#     time.sleep(2)
-
-
-# # High emergency alert color: red 
-# while True: 
-#     red.duty_cycle = duty_cycle(50)
-#     time.sleep(2)
-
-# # # Medium emergency alert color: orange 
-# while True: 
-#     red.duty_cycle = duty_cycle(30)
-#     blue.duty_cycle = duty_cycle(1)
-#     green.duty_cycle = duty_cycle(7)
-#     time.sleep(2)
-
-# # Low emergency alert color: yellow 
-while True: 
-    red.duty_cycle = duty_cycle(20)
-    blue.duty_cycle = duty_cycle(0)
-    green.duty_cycle = duty_cycle(10)
-    time.sleep(2)
 
 
 
