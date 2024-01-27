ae36a319fe863b9e59d242586a98b2f493bfbea2
rcheeter <robert.heeter@rice.edu>
2024-01-27 16:09:34 -0600

    Update linear_actuator_OLD.py

--- prototyping/compression/linear_actuator_OLD.py
+++ prototyping/compression/linear_actuator_OLD.py
@@ -81,31 +81,31 @@ class LinearActuator():
 
 # testing
 if __name__ == '__main__':
-    # la = LinearActuator(pins=[10, 9, 11], freq=1000, verbose=True)
+    la = LinearActuator(pins=[10, 9, 11], freq=1000, verbose=True)
 
-    GPIO.cleanup()
+    # GPIO.cleanup()
 
-    GPIO.setmode(GPIO.BCM)
+    # GPIO.setmode(GPIO.BCM)
 
-    GPIO.setup(9, GPIO.OUT)
-    GPIO.setup(11, GPIO.OUT)
-    GPIO.setup(10, GPIO.OUT)
+    # GPIO.setup(9, GPIO.OUT)
+    # GPIO.setup(11, GPIO.OUT)
+    # GPIO.setup(10, GPIO.OUT)
     
-    GPIO.output(9, GPIO.HIGH)
-    GPIO.output(11, GPIO.HIGH)
-    GPIO.output(10, GPIO.LOW)
+    # GPIO.output(9, GPIO.HIGH)
+    # GPIO.output(11, GPIO.HIGH)
+    # GPIO.output(10, GPIO.LOW)
 
-    time.sleep(20)
-    # la.run('forward', 100, 4)
-    # time.sleep(1)
-    # la.run('forward', 50, 4)
-    # time.sleep(1)
-    # la.run('forward', 25, 4)
-    # time.sleep(1)
-    # la.run('forward', 15, 4)
-    # time.sleep(1)
-    # la.run('forward', 5, 4)
-    # time.sleep(1)
+    # time.sleep(20)
+    la.run('forward', 100, 4)
+    time.sleep(1)
+    la.run('forward', 50, 4)
+    time.sleep(1)
+    la.run('forward', 25, 4)
+    time.sleep(1)
+    la.run('forward', 15, 4)
+    time.sleep(1)
+    la.run('forward', 5, 4)
+    time.sleep(1)
 
     # time.sleep(1) # wait
     # la.run('backward', 60, 4)
