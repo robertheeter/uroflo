ab643da8051dc62a90731e142f6511266aaaf73c
rcheeter <robert.heeter@rice.edu>
2024-01-29 14:28:04 -0600

    yeehaw

--- prototyping/notification/code_8_OLD.py
+++ prototyping/notification/code_8_OLD.py
@@ -1,72 +1,93 @@
+
+
+
+
+
+
+
+
+
+# do not use!!!!
+
+
+
+
+
+
+
+
+
+
+
 # SPDX-FileCopyrightText: 2017 Tony DiCola for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
 
 # Importing libraries 
-import time
-import board
-import pwmio
-import pygame
+# import time
+# import board
+# import pwmio
+# import pygame
 
-# Defining pins on board GPIOs 
-RED_PIN = board.D8  # Red LED pin
-GREEN_PIN = board.D7  # Green LED pin
-BLUE_PIN = board.D1  # Blue LED pin
+# # Defining pins on board GPIOs 
+# RED_PIN = board.D8  # Red LED pin
+# GREEN_PIN = board.D7  # Green LED pin
+# BLUE_PIN = board.D1  # Blue LED pin
 
-# Define PWM outputs:
-red = pwmio.PWMOut(RED_PIN)
-green = pwmio.PWMOut(GREEN_PIN)
-blue = pwmio.PWMOut(BLUE_PIN)
+# # Define PWM outputs:
+# red = pwmio.PWMOut(RED_PIN)
+# green = pwmio.PWMOut(GREEN_PIN)
+# blue = pwmio.PWMOut(BLUE_PIN)
 
-# Function to simplify setting duty cycle to percent value.
-def duty_cycle(percent):
-    return int(percent / 100.0 * 65535.0)
+# # Function to simplify setting duty cycle to percent value.
+# def duty_cycle(percent):
+#     return int(percent / 100.0 * 65535.0)
     
-# # default standard blue color
-def default_LED():
-    red.duty_cycle = duty_cycle(1)
-    green.duty_cycle = duty_cycle(2.74)
-    blue.duty_cycle = duty_cycle(4)
+# # # default standard blue color
+# def default_LED():
+#     red.duty_cycle = duty_cycle(1)
+#     green.duty_cycle = duty_cycle(2.74)
+#     blue.duty_cycle = duty_cycle(4)
 
-# red - high emergency
-def red_LED():
-    red.duty_cycle = duty_cycle(50)
-    blue.duty_cycle = duty_cycle(0)
-    green.duty_cycle = duty_cycle(0)
+# # red - high emergency
+# def red_LED():
+#     red.duty_cycle = duty_cycle(50)
+#     blue.duty_cycle = duty_cycle(0)
+#     green.duty_cycle = duty_cycle(0)
 
-# orange - medium emergency
-def orange_LED():
-    red.duty_cycle = duty_cycle(50)
-    blue.duty_cycle = duty_cycle(0)
-    green.duty_cycle = duty_cycle(10)
+# # orange - medium emergency
+# def orange_LED():
+#     red.duty_cycle = duty_cycle(50)
+#     blue.duty_cycle = duty_cycle(0)
+#     green.duty_cycle = duty_cycle(10)
     
-# yellow - low emergency 
-def yellow_LED():
-    red.duty_cycle = duty_cycle(20)
-    blue.duty_cycle = duty_cycle(0)
-    green.duty_cycle = duty_cycle(10)
+# # yellow - low emergency 
+# def yellow_LED():
+#     red.duty_cycle = duty_cycle(20)
+#     blue.duty_cycle = duty_cycle(0)
+#     green.duty_cycle = duty_cycle(10)
 
-def mellow_alarm():
-    pygame.init()
-    my_sound = pygame.mixer.Sound('sonar.mp3') # only plays once
-    my_sound.play()
-    my_sound.set_volume(0.5) # can set sound volume from 0-1 
+# def mellow_alarm():
+#     pygame.init()
+#     my_sound = pygame.mixer.Sound('sonar.mp3') # only plays once
+#     my_sound.play()
+#     my_sound.set_volume(0.5) # can set sound volume from 0-1 
 
-def harsh_alarm():
-    my_sound = pygame.mixer.Sound('harsh.mp3') # repeats 4 times 
-    my_sound.play()
-    my_sound.set_volume(0.5) # can set sound volume from 0-1 
+# def harsh_alarm():
+#     my_sound = pygame.mixer.Sound('harsh.mp3') # repeats 4 times 
+#     my_sound.play()
+#     my_sound.set_volume(0.5) # can set sound volume from 0-1 
 
-while True:
-    FADE_SLEEP = 15
-    default_LED()
-    time.sleep(FADE_SLEEP / 10)
-    yellow_LED()
-    time.sleep(FADE_SLEEP / 10)
-    orange_LED()
-    time.sleep(FADE_SLEEP / 10)
-    red_LED()
-    time.sleep(FADE_SLEEP / 10)
+# while True:
+#     FADE_SLEEP = 15
+#     default_LED()
+#     time.sleep(FADE_SLEEP / 10)
+#     yellow_LED()
+#     time.sleep(FADE_SLEEP / 10)
+#     orange_LED()
+#     time.sleep(FADE_SLEEP / 10)
+#     red_LED()
+#     time.sleep(FADE_SLEEP / 10)
 
     
 
