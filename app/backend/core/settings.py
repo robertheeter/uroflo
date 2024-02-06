aa255a65bfbdc2f024f8db66d63e96058e73bd38
rcheeter <robert.heeter@rice.edu>
2024-02-05 21:49:17 -0600

    asdf

--- app/backend/core/settings.py
+++ app/backend/core/settings.py
@@ -52,10 +52,10 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['http://localhost:5173'] # replace with React frontend URL
-CSRF_COOKIE_SECURE = False
-CSRF_COOKIE_HTTPONLY = False
-CSRF_COOKIE_NAME = 'csrftoken'
+# CSRF_TRUSTED_ORIGINS = ['http://localhost:5173'] # replace with React frontend URL
+# CSRF_COOKIE_SECURE = False
+# CSRF_COOKIE_HTTPONLY = False
+# CSRF_COOKIE_NAME = 'csrftoken'
 
 # CORS_ORIGIN_WHITELIST = [
 #     'http://localhost:8000',
