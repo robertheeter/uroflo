627e4646271fc42dcfb3f884429ff75d42caaf92
rcheeter <robert.heeter@rice.edu>
2024-02-05 20:59:01 -0600

    Update settings.py

--- app/backend/core/settings.py
+++ app/backend/core/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
 
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = ['http://localhost:5173']
 
 # CORS_ORIGIN_WHITELIST = [
 #     'http://localhost:8000',
