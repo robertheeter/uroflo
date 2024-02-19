a62c6f1435f0dac7a28b5bf5641d3ee8cf1075b0
rcheeter <robert.heeter@rice.edu>
2024-02-19 17:12:54 -0600

    Update linear_actuator.py

--- linear_actuator.py
+++ linear_actuator.py
@@ -101,8 +101,9 @@ if __name__ == '__main__':
 
     linear_actuator.retract(duty_cycle=100, duration=4)
     linear_actuator.extend(duty_cycle=100, duration=8)
-    time.sleep(4)
+    
     print("FULLY OCCLUDED")
+    time.sleep(4)
 
     for i in range(1000):
         input("PRESS ENTER TO INCREASE FLOW")
