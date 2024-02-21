af84fec381787fec590a1f07f3f7882ae37d93d8
rcheeter <robert.heeter@rice.edu>
2024-02-20 19:14:13 -0600

    Update linear_actuator.py

--- app/backend/system/components/linear_actuator.py
+++ app/backend/system/components/linear_actuator.py
@@ -126,8 +126,8 @@ if __name__ == '__main__':
 
     while True:
         input = user_input("\nINPUT: 'r'/'i' to INCREASE FLOW, 'e'/'d' to DECREASE FLOW, 'q' to QUIT: ")
-        print()
-        
+        print("\n")
+
         if input in ['r','i']:
             increase += 1
             print(f"increase count = {increase}")
