e1825fc47415b7c78182eb7c1648bd3bdc336df7
achabbi <aac17@rice.edu>
2024-02-22 23:42:15 -0600

    pid testing

--- app/backend/system/other/pid.py
+++ app/backend/system/other/pid.py
@@ -5,40 +5,36 @@ Notes
 - 
 
 Documentation
-- 
+
+For tuning:
+https://www.thorlabs.com/newgrouppage9.cfm?objectgroup_id=9013
 
 '''
-
 import time
-import numpy as np
 from simple_pid import PID
 
-pid = PID(1, 0.1, 0.05, setpoint=1)
 
-# Assume we have a system we want to control in controlled_system
-v = controlled_system.update(0)
+def get_hematuria(idx):
+    levels = list(range(46, 58, 2))
+    levels.extend(list(reversed(range(46, 58, 2))))
+    return levels[idx]
+
+
+Kp = 1
+Ki = 0.1
+Kd = 0.05
+hematuria_setpoint = 56
+
+pid = PID(Kp, Ki, Kd, setpoint=hematuria_setpoint, output_limits=(0, 15))
+# use output_limits to contrain the output to a range
+
+i = 0
 
 while True:
     # Compute new output from the PID according to the systems current value
-    control = pid(v)
+    h = get_hematuria(i)
+    control = pid(h)
 
-    # Feed the PID output to the system and get its current value
-    v = controlled_system.update(control)
-
-
-def PID():
-    pass
-
-
-# PID
-'''
-use this lol
-- https://simple-pid.readthedocs.io/en/latest/index.html
-
-'''
-
-
-
-
-if __name__ == '__main__':
-    PID()
+    print(f'Hematuria: {h}, Control: {control}')
+    time.sleep(1)
+    i += 1
