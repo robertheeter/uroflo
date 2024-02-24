9ba7d5d0c9560183fcfa1f05e36fe437995f51a2
achabbi <aac17@rice.edu>
2024-02-23 18:48:31 -0600

    Create test.py

---
+++ app/backend/system/components/test.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set the GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Set the GPIO pin for the button
+button_pin = 10  # You can change this to the desired GPIO pin
+
+# Set up the button pin as an input with a pull-up resistor
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+try:
+    print("Press the button (CTRL+C to exit):")
+    while True:
+        if GPIO.input(button_pin) == GPIO.LOW:
+            print("Button pressed!")
+            # You can add your desired actions here
+            time.sleep(0.2)  # Add a small delay to debounce the button
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()  # Cleanup GPIO settings when exiting the script
