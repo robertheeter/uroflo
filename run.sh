18b72abbe94e1fc99dc44ed9d6314248201bd15f
rcheeter <robert.heeter@rice.edu>
2024-02-26 16:56:09 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,21 +3,32 @@
 # activate environment
 # cd Documents/uroflo
 source venv/bin/activate
+wait 1
 
-# run in parallel
-# (
-  # cd app/backend || exit
-  # python manage.py runserver
-  # python system/main.py &
-  # python system/hematuria.py &
-  # python system/mass.py &
-# )
-
+# run frontend
 (
   cd app/frontend || exit
   npm run dev &
 )
 
-firefox-esr -private --kiosk http://localhost:5173/ # quit firefox-esr kiosk mode via ALT+F4; install firefox-esr via 'sudo apt install firefox-esr'
+# run backend
+(
+  cd app/backend || exit
+  python manage.py runserver &
+  
+)
+
+# run system
+(
+  cd app/backend/system || exit
+  python system/main.py &
+  python system/hematuria.py &
+)
+
+# run firefox-esr
+firefox-esr -private --kiosk http://localhost:5173/
+  # quit firefox-esr kiosk mode via ALT+F4
+  # move forward or back page in firefox-esr kiosk mode via ALT+[left arrow/right arrow]
+  # install firefox-esr via 'sudo apt install firefox-esr'
 
 wait
