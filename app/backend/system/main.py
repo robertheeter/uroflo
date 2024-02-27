f1004e5e4385b1ae270ecea41b89ba3fb71a82d6
rcheeter <robert.heeter@rice.edu>
2024-02-26 18:06:07 -0600

    Update main.py

--- app/backend/system/main.py
+++ app/backend/system/main.py
@@ -239,7 +239,7 @@ def main():
         
         while True:
             if val > supply_replace_count_added:
-                time.sleep(0.1)
+                time.sleep(0.5)
                 supply_replace_count_added = val
                 supply_replace_volume = get_data(key='supply_replace_volume', file='interface')
                 supply_weight_sensor.calibrate(known_mass=supply_replace_volume, replicates=SUPPLY_WEIGHT_SENSOR_REPLICATES) # calibrate weight sensor with known mass
@@ -260,7 +260,7 @@ def main():
         while True:
             val = get_data(key='waste_replace_count_added', file='interface')
             if  val > waste_replace_count_added:
-                time.sleep(0.1)
+                time.sleep(0.5)
                 waste_replace_count_added = val
                 waste_replace_volume = get_data(key='waste_replace_volume', file='interface')
                 waste_weight_sensor.calibrate(known_mass=waste_replace_volume, replicates=WASTE_WEIGHT_SENSOR_REPLICATES) # calibrate weight sensor with known mass
