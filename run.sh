1b4e3ccdc05d52d7fc4ab287dfd413c4f074c799
rcheeter <robert.heeter@rice.edu>
2024-03-31 16:19:20 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,38 +9,40 @@ source venv/bin/activate
 # delete all existing databases
 cd app/backend/system
 sudo rm -r data
-sleep 1.0
-sudo mkdir data
+sleep 1
+mkdir data
 cd ../..
-sleep 1.0
+sleep 1
 
 # run frontend
 (
   cd frontend || exit
   npm run dev &
-  sleep 1.0
+  sleep 1
 )
 
 # run system
 (
   cd backend/system || exit
   python main.py &
+  sleep 1
   python hematuria.py &
-  sleep 1.0
+  sleep 1
+  sleep 1
 )
 
 # run backend
 (
   cd backend || exit
   python manage.py runserver &
-  sleep 1.0
+  sleep 1
 )
 
 # run browser
 (
   cd frontend/browser || exit
   python kiosk.py
-  sleep 1.0
+  sleep 1
 )
 
 wait
